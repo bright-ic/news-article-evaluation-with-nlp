d71fbcd65dc48429be9b91455d90d8913f3f03e4
Bright Onwukwe <onwukweb@gmail.com>
2020-03-31 10:13:44 +0100

    Setup webpack and sass loader

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,16 @@
 import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
+import { handleSubmit } from './js/formHandler';
+import "./styles/base.css";
+import "./styles/footer.css";
+import "./styles/form.css";
+import "./styles/header.css";
+import "./styles/resets.css";
 
 console.log(checkForName);
 
-alert("I EXIST")
 console.log("CHANGE!!");
+
+export {
+  checkForName,
+  handleSubmit
+}
